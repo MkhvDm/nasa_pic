ccdb0b2b0f7b65e3d9be6ba8233281a5002a6c9b
Mikheev <mkhvdm@gmail.com>
2023-02-16 19:01:49 +0300

    tries buttons

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import os
 import time
 import logging
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
 
 from dotenv import load_dotenv
 
@@ -20,17 +20,41 @@ BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 ENDPOINT = ''
 
+def get_start_menu():
+    keyboard = [
+        [
+            InlineKeyboardButton("Start button", callback_data="1"),
+            InlineKeyboardButton("Help", callback_data="2"),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return reply_markup
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data="1"),
+            InlineKeyboardButton("Option 2", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data="3")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
     # todo get or create user
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text='Hi, no pic yet...'
-    )
+    
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+    # await context.bot.send_message(
+    #     chat_id=update.effective_chat.id,
+    #     text='Hi, no pic yet...'
+    # )
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 async def get_pic(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
@@ -42,9 +66,11 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
+    application.add_handler(CallbackQueryHandler(button))
+
     get_pic_handler = CommandHandler('get', get_pic)
 
-    application.add_handler()
+    application.add_handler(get_pic_handler)
     application.add_handler(start_handler)
 
     application.run_polling()
